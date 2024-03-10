fc559715fa04a97a9aadbdb6c8175ecf329db96a
Nakul-Thureja <nakul20528@iiitd.ac.in>
2024-03-10 18:21:06 +0530

    multithreaded scraping

--- utils.py
+++ utils.py
@@ -31,55 +31,68 @@ cookies = {
     'csm-hit': 'tb:s-RCA08544A2YQCT2J63XM|1684211284757&t:1684211285532&adb:adblk_no',
 }
 
-headers = {
-            'User-Agent': specific_string(random.randint(1,999)),
-            'From': specific_string(random.randint(1,999))
-}
 
 
 #WORKING FUNCTIONS
 
 def scrape_data(url):
-    response = session.get(url, headers=headers,cookies=cookies)
-    print(response.status_code)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    return soup
+    try:
+        headers = {
+                    'User-Agent': specific_string(random.randint(1,999)),
+                    'From': specific_string(random.randint(1,999))
+        }
+        response = session.get(url, headers=headers,cookies=cookies)
+        print(response.status_code)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup
+    except:
+        return BeautifulSoup()
 
 def get_product_details(soup):
     product_details = {}
+    try:
+        name_element = soup.find('span', {'id':'productTitle'})
+        if name_element is not None:
+            product_details['name'] = name_element.string.strip()
 
-    name_element = soup.find('span', {'id':'productTitle'})
-    if name_element is not None:
-        product_details['name'] = name_element.string.strip()
+        price_element = soup.find('span', {'class':'a-offscreen'})
+        if price_element is not None:
+            product_details['price'] = price_element.string.strip()
 
-    price_element = soup.find('span', {'class':'a-offscreen'})
-    if price_element is not None:
-        product_details['price'] = price_element.string.strip()
-
-    return product_details
-
+        return product_details
+    except:
+        return product_details
+    
 # 3. Scrape reviews
 def get_all_reviews(url):
     reviews = []
-    headers = {
-                'User-Agent': specific_string(random.randint(1,999)),
-                'From': specific_string(random.randint(1,999))
-    }
+    try:
+        headers = {
+                    'User-Agent': specific_string(random.randint(1,999)),
+                    'From': specific_string(random.randint(1,999))
+        }
 
-    response = requests.get(url, headers=headers, cookies = cookies)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    reviews.extend([review.text.strip() for review in soup.find_all('span', {'data-hook': 'review-body'})])
-    return reviews
-
+        response = requests.get(url, headers=headers, cookies = cookies)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        reviews.extend([review.text.strip() for review in soup.find_all('span', {'data-hook': 'review-body'})])
+        return reviews
+    except:
+        return reviews
+    
 def scrape_reviews(url):
-    response = requests.get(url, headers=headers, cookies=cookies)
-    final_url = response.url  # Get the final URL after all redirects
-    reviews_url = final_url.replace("/dp/", "/product-reviews/") + "?pageNumber=" + str(1)
-    # reviews_url = url.replace("/sspa/", "/product-reviews/") + "?pageNumber=" + str(1)
-    return get_all_reviews(reviews_url)
+    try:
+        headers = {
+                    'User-Agent': specific_string(random.randint(1,999)),
+                    'From': specific_string(random.randint(1,999))
+        }
 
-
-
+        response = requests.get(url, headers=headers, cookies=cookies)
+        final_url = response.url  # Get the final URL after all redirects
+        reviews_url = final_url.replace("/dp/", "/product-reviews/") + "?pageNumber=" + str(1)
+        # reviews_url = url.replace("/sspa/", "/product-reviews/") + "?pageNumber=" + str(1)
+        return get_all_reviews(reviews_url)
+    except:
+        return []
 
 # 4. NLP-based review analysis
 def analyze_reviews(reviews):
@@ -94,41 +107,49 @@ def analyze_reviews(reviews):
     return average_compound_score
 
 def get_about_this_item(soup):
-    about_item = soup.find('h1', {'class':'a-size-base-plus a-text-bold'}).text
-    item_details = [li.text.strip() for li in soup.find_all('li', {'class':'a-spacing-mini'})]
+    try:
+        about_item = soup.find('h1', {'class':'a-size-base-plus a-text-bold'}).text
+        item_details = [li.text.strip() for li in soup.find_all('li', {'class':'a-spacing-mini'})]
 
-    return about_item, item_details
-
+        return about_item, item_details
+    except:
+        return [], []
 
 
 
 def search_amazon(product_name):
-    # Replace spaces in the product name with '+'
-    product_name = product_name.replace(' ', '+')
+    try:
+        # Replace spaces in the product name with '+'
+        product_name = product_name.replace(' ', '+')
 
-    # Create the URL for the search results page
-    url = f"https://www.amazon.in/s?k={product_name}"
+        # Create the URL for the search results page
+        url = f"https://www.amazon.in/s?k={product_name}"
 
-    # # Define the proxies
-    # proxies = {
-    #     "http": "http://10.10.1.10:3128",
-    #     "https": "http://10.10.1.10:1080",
-    # }
+        # # Define the proxies
+        # proxies = {
+        #     "http": "http://10.10.1.10:3128",
+        #     "https": "http://10.10.1.10:1080",
+        # }
+        headers = {
+                    'User-Agent': specific_string(random.randint(1,999)),
+                    'From': specific_string(random.randint(1,999))
+        }
 
-    # Send a GET request to the Amazon server
-    #headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-    response = requests.get(url, cookies = cookies, headers=headers)
+        # Send a GET request to the Amazon server
+        #headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+        response = requests.get(url, cookies = cookies, headers=headers)
 
-    # Parse the response text with BeautifulSoup
-    soup = BeautifulSoup(response.text, 'html.parser')
+        # Parse the response text with BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find all product links on the search results page
-    product_links = soup.find_all('a', {'class': 'a-link-normal a-text-normal'})
+        # Find all product links on the search results page
+        product_links = soup.find_all('a', {'class': 'a-link-normal a-text-normal'})
 
-    # Extract the href attribute of each link and prepend 'https://www.amazon.com' to get the full URL
-    product_urls = ['https://www.amazon.in' + link['href'] for link in product_links]
-    return product_urls
-
+        # Extract the href attribute of each link and prepend 'https://www.amazon.com' to get the full URL
+        product_urls = ['https://www.amazon.in' + link['href'] for link in product_links]
+        return product_urls
+    except:
+        return []
 # Test the function
 # product_urls = search_amazon('iphone')
 # for url in product_urls:
@@ -137,47 +158,59 @@ def search_amazon(product_name):
 
 
 def get_soup(params, page_number):
-    params['page'] = str(page_number)
-    response = requests.get('https://www.amazon.in/s', params=params, cookies=cookies, headers=headers)
-    return BeautifulSoup(response.text, 'html.parser')
-
-def get_star_elements(soup):
-    return soup.find_all(class_='a-icon-star-small')
-
-def get_price_and_image_url(url):
-    headers = {
+    try:
+        params['page'] = str(page_number)
+        headers = {
                 'User-Agent': specific_string(random.randint(1,999)),
                 'From': specific_string(random.randint(1,999))
-    }
+        }
+        response = requests.get('https://www.amazon.in/s', params=params, cookies=cookies, headers=headers)
+        return BeautifulSoup(response.text, 'html.parser')
+    except:
+        return BeautifulSoup()
+    
+def get_star_elements(soup):
+    try:
+        return soup.find_all(class_='a-icon-star-small')
+    except:
+        return []
+    
+def get_price_and_image_url(url):
+    try:
+        headers = {
+                    'User-Agent': specific_string(random.randint(1,999)),
+                    'From': specific_string(random.randint(1,999))
+        }
 
-    response = requests.get(url, headers=headers, cookies = cookies)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # print(soup)
-    price_element = soup.find('span', {'class':'a-offscreen'})
-    if price_element is not None:
-        price = price_element.string.strip()    
-    script_element = soup.find('script', text=re.compile('var iUrl'))
+        response = requests.get(url, headers=headers, cookies = cookies)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # print(soup)
+        price_element = soup.find('span', {'class':'a-offscreen'})
+        if price_element is not None:
+            price = price_element.string.strip()    
+        script_element = soup.find('script', text=re.compile('var iUrl'))
 
-    # Extract the image URL from the script element
-    if script_element is not None:
-        match = re.search(r'var iUrl = "(.*?)";', script_element.string)
-        if match:
-            image_url = match.group(1)
+        # Extract the image URL from the script element
+        if script_element is not None:
+            match = re.search(r'var iUrl = "(.*?)";', script_element.string)
+            if match:
+                image_url = match.group(1)
+            else:
+                image_url = None
         else:
             image_url = None
-    else:
-        image_url = None
-    return price, image_url
-
-
-
-
+        return price, image_url
+    except:
+        return None, None
 
 def get_item_from_star_element(star_element):
-    item = star_element.parent.parent.parent.parent.parent.parent.parent.parent.parent
-    a_tag = item.find('a', class_='a-link-normal')
-    if a_tag is not None:
-        url = 'https://www.amazon.in' + a_tag.get('href')
-    else:
-        url = None
-    return item, url
+    try:
+        item = star_element.parent.parent.parent.parent.parent.parent.parent.parent.parent
+        a_tag = item.find('a', class_='a-link-normal')
+        if a_tag is not None:
+            url = 'https://www.amazon.in' + a_tag.get('href')
+        else:
+            url = None
+        return item, url
+    except:
+        return None, None
--- website.py
+++ website.py
@@ -8,6 +8,32 @@ st.markdown('#### E-Commerce Competitive Analysis System')
 st.markdown('Search for a product on Amazon')
 key = st.text_input('Enter the product name')
 
+from concurrent.futures import ThreadPoolExecutor
+import requests
+from PIL import Image
+from io import BytesIO
+import concurrent.futures
+
+def process_element(index, element):
+    try:
+        item, url = get_item_from_star_element(element)
+        image = item.find('img', class_='s-image')
+        response = requests.get(image['src'])
+        img_pil = Image.open(BytesIO(response.content))
+
+        soup = scrape_data(url)
+        product_details = get_product_details(soup)
+
+        description = get_about_this_item(soup)
+
+        reviews = scrape_reviews(url)
+        average_compound_score = analyze_reviews(reviews)
+        product_detail = [product_details['name'], " ".join(description[1]), img_pil, url]
+        all_detail = [product_details['name'], product_details['price'], description[0], description[1], average_compound_score, reviews,url, image['src']]
+        return index, product_detail, all_detail
+    except:
+        return index, [], []
+    
 def get_data(key):
     with st.spinner('Searching for products...'):
         params = {
@@ -15,95 +41,72 @@ def get_data(key):
         }
         page = get_soup(params, 1)
 
-
         elements = get_star_elements(page)
         
         product_dict = {}
         all_detail_product = {}
-        for index, element in enumerate(elements):
-            item, url = get_item_from_star_element(element)
-            image = item.find('img', class_='s-image')
-            response = requests.get(image['src'])
-            img_pil = Image.open(BytesIO(response.content))
-            # left_co, cent_co,last_co = st.columns(3)
-            # with left_co:
-            #     st.image(image['src'], width=300)  # Display the product image and URL
-            # with last_co:
-            #     st.write(url)
-            # price, imagesrc = get_price_and_image_url(url)
-            # st.image(imagesrc, width=300)
 
-            soup = scrape_data(url)
-            product_details = get_product_details(soup)
-            # print(index)
-            
-            # st.write(product_details)  # Display the product details
+        # Create a ThreadPoolExecutor
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            # Use list comprehension to create a list of futures
+            futures = [executor.submit(process_element, index, element) for index, element in enumerate(elements)]
 
-            description = get_about_this_item(soup)
-            # st.write(f'About this item: {description}')  # Display the "About this item" section
-
-            reviews = scrape_reviews(url)
-            average_compound_score = analyze_reviews(reviews)
-            product_dict[index] = [product_details['name'], " ".join(description[1]), img_pil, url]
-            # st.markdown(f"**Average sentiment score:** {average_compound_score:.2f}")
-            # st.markdown(f"**Overall sentiment:** {'positive' if average_compound_score > 0 else 'negative' if average_compound_score < 0 else 'neutral'}")
-
-            # df = pd.DataFrame(reviews, columns=['Review'])
-
-            
-        # Display the DataFrame on Streamlit
-            # st.dataframe(df)
-            # st.write(reviews)  # Display the reviews
-            # reviews = scrape_reviews(url)
-            # analyze_reviews(reviews)
-            all_detail_product[index] = [product_details['name'], product_details['price'], description[0], description[1], average_compound_score, reviews,url, image['src']]
-            # print(all_detail_product[index])
+            # Wait for all futures to complete
+            for future in concurrent.futures.as_completed(futures):
+                index, product_detail, all_detail = future.result()
+                product_dict[index] = product_detail
+                all_detail_product[index] = all_detail
         max_index, top_k_recommendations = get_top_k_recommendations(key, product_dict)
         print("returned")
-
         # reviews = scrape_reviews(product_dict[max_index][3])
         # average_compound_score = analyze_reviews(reviews)
         # price, image_url = get_price_and_image_url(product_dict[max_index][3])
         # df = pd.DataFrame(reviews, columns=['Review'])
+    
     with st.sidebar:
-        st.title("Your Product")
-        st.markdown(f"<div style='text-align: center; padding: 10px;'><img src='{all_detail_product[max_index][7]}' style='width: 80%;'></div>",unsafe_allow_html=True)    
-        st.markdown(f'<div style="text-align: center;"><a href="{all_detail_product[max_index][6]}" target="_blank">Go to product page</a>', unsafe_allow_html=True)   
+        try:
+            st.title("Your Product")
+            st.markdown(f"<div style='text-align: center; padding: 10px;'><img src='{all_detail_product[max_index][7]}' style='width: 80%;'></div>",unsafe_allow_html=True)    
+            st.markdown(f'<div style="text-align: center;"><a href="{all_detail_product[max_index][6]}" target="_blank">Go to product page</a>', unsafe_allow_html=True)   
 
-        st.markdown(f"## {all_detail_product[max_index][0]}")
-        st.markdown(f"### Price: {all_detail_product[max_index][1]}")
-        st.markdown(f"### Average sentiment score: {all_detail_product[max_index][4]:.2f}")
-        st.markdown(f"### Overall sentiment: {'positive' if all_detail_product[max_index][4] > 0 else 'negative' if all_detail_product[max_index][4] < 0 else 'neutral'}")
-        with st.expander("About the Product"):
-            st.write(pd.DataFrame(all_detail_product[max_index][3], columns=['About the Product']))
-        with st.expander("Show Reviews"):
-            st.dataframe(pd.DataFrame(all_detail_product[max_index][5], columns=['Review']))
-
+            st.markdown(f"## {all_detail_product[max_index][0]}")
+            st.markdown(f"### Price: {all_detail_product[max_index][1]}")
+            st.markdown(f"### Average sentiment score: {all_detail_product[max_index][4]:.2f}")
+            st.markdown(f"### Overall sentiment: {'positive' if all_detail_product[max_index][4] > 0 else 'negative' if all_detail_product[max_index][4] < 0 else 'neutral'}")
+            with st.expander("About the Product"):
+                st.write(pd.DataFrame(all_detail_product[max_index][3], columns=['About the Product']))
+            with st.expander("Show Reviews"):
+                st.dataframe(pd.DataFrame(all_detail_product[max_index][5], columns=['Review']))
+        except:
+            st.write("No product found")
+    
     st.markdown(f"### Top {len(top_k_recommendations)} products by Competition")
     tab_labels = [f"Product {i+1}" for i in range(len(top_k_recommendations))]
 
-    tabs = st.tabs(tab_labels)
-    count = 0
-    for index in top_k_recommendations:
-        with tabs[count]:
-            count+=1
-            with st.spinner(f"Fetching data for {product_dict[index][0]}..."):
- 
-                left_co, cent_co,last_co = st.columns(3)
-                with left_co:
-                    st.image(all_detail_product[index][7], width=300)
-                    st.markdown(f'<a href="{all_detail_product[index][6]}" target="_blank">Go to product page</a>', unsafe_allow_html=True)
-                with last_co:
-                    st.markdown(f"##### {all_detail_product[index][0]}")
-                    st.markdown(f"**Price:** {all_detail_product[index][1]}")
-                    st.markdown(f"**Average sentiment score:** {all_detail_product[index][4]:.2f}")
-                    st.markdown(f"**Overall sentiment:** {'positive' if all_detail_product[index][4] > 0 else 'negative' if all_detail_product[index][4] < 0 else 'neutral'}")
-                with st.expander("About the Product"):
-                    st.write(pd.DataFrame(all_detail_product[index][3], columns=['About the Product']))
-                with st.expander("Show Reviews"):
-                    st.dataframe(pd.DataFrame(all_detail_product[index][5], columns=['Review']))
-
-        
+    try:
+        tabs = st.tabs(tab_labels)
+        count = 0
+        for index in top_k_recommendations:
+            with tabs[count]:
+                count+=1
+                with st.spinner(f"Fetching data for {product_dict[index][0]}..."):
+    
+                    left_co, cent_co,last_co = st.columns(3)
+                    with left_co:
+                        st.image(all_detail_product[index][7], width=300)
+                        st.markdown(f'<a href="{all_detail_product[index][6]}" target="_blank">Go to product page</a>', unsafe_allow_html=True)
+                    with last_co:
+                        st.markdown(f"##### {all_detail_product[index][0]}")
+                        st.markdown(f"**Price:** {all_detail_product[index][1]}")
+                        st.markdown(f"**Average sentiment score:** {all_detail_product[index][4]:.2f}")
+                        st.markdown(f"**Overall sentiment:** {'positive' if all_detail_product[index][4] > 0 else 'negative' if all_detail_product[index][4] < 0 else 'neutral'}")
+                    with st.expander("About the Product"):
+                        st.write(pd.DataFrame(all_detail_product[index][3], columns=['About the Product']))
+                    with st.expander("Show Reviews"):
+                        st.dataframe(pd.DataFrame(all_detail_product[index][5], columns=['Review']))
+    except:
+        pass
+            
 
     
     # st.sidebar.title("Your Product")
